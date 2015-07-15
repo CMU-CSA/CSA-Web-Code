4ae361daf0bcbf5e3aeb2be151a30c5d91a47fde
law-liet <lawliet@orion.codes>
2015-07-16 01:09:10 +0800

    [Add] views.py/contact, aboutus; urls.py | Add render functions and routes

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^aboutus/', views.aboutus, name='aboutus'),
 ]
 
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def home(request):
     return render(request,'mainpage/home.html')
 
+def contact(request):
+    return render(request,'mainpage/contact.html')
+
+def aboutus(request):
+    return render(request,'mainpage/aboutus.html')
