2fd865cbf2a85deb2f1ae6a9fb79674c5b06d999
law-liet <lawliet@orion.codes>
2015-07-30 14:22:47 +0800

    [Modify] activities.html & style.css | improve responsive effect, almost perfect

--- mainpage/static/mainpage/home.js
+++ mainpage/static/mainpage/home.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
 
+    resizeActivityImage();
+
+    $(window).resize(function(){
+        resizeActivityImage();
+    });
+
 });
+
+function resizeActivityImage() {
+
+    $(".activity").each(function(){
+
+        var width = $(".pic").width();
+        var newHeight = width*2/3;
+
+        $(this).find(".pic").height(newHeight);
+        if ($(window).width() < 768) {
+            $(this).find(".words").height(newHeight-30);
+            $(this).height(2*newHeight-20);
+        } else {
+            $(this).find(".words").height(newHeight-20);
+            $(this).height(newHeight);
+        }
+    });
+}
