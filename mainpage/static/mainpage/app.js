0ef350256dcd14862292f26654c94b8b51ad3a5f
George Liu <liuchuan95@gmail.com>
2015-11-11 23:28:46 -0500

    fix bug of reponsiveness of side-menu

--- mainpage/static/mainpage/app.js
+++ mainpage/static/mainpage/app.js
@@ -8,10 +8,21 @@ $(document).ready(function(){
  		window.alert('Sorry, login functionality is under construction');
  		console.log('you clikc it');
  		return false;
- 	})
+ 	});
 
 
+ 	$('#side-menu-collapse').click(function(){
+ 		$('.csa-side-menu').toggleClass('active');
+ 	});
 
+ 	$('.body.click').click(function(event){
+ 		if ($('.csa-side-menu').hasClass('active')){
+ 			 var x = event.pageX;
+ 			 var width = document.documentElement.clientWidth;
+ 			 if (x < width - 275){
+ 			 	$('.csa-side-menu').toggleClass('active');
+ 			 }
+ 		}
 
-
+ 	});
 });
