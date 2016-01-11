c840c33672c8ba594403c29a19aa04d88fd4dce8
George Liu <liuchuan95@gmail.com>
2016-01-11 11:02:00 -0500

    finish side-menu transition, all bug fixed

--- mainpage/static/mainpage/app.js
+++ mainpage/static/mainpage/app.js
@@ -6,18 +6,17 @@ $(document).ready(function(){
  	$('#loginSubmit').submit(function(){
  		/* should shoot an ajax post request to server with email and password*/
  		window.alert('Sorry, login functionality is under construction');
- 		console.log('you clikc it');
+ 		console.log('you click it');
  		return false;
  	});
 
 
  	$('#csa-side-menu-button').click(function(){
- 		$('.csa-side-menu').toggleClass('active');
- 		$('.csa-content').toggleClass('inactive');
+ 		$('.base-container').toggleClass('active');
  	});
 
  	$('body').click(function(e){
- 		if ($('.csa-side-menu').hasClass('active')){
+ 		if ($('.base-container').hasClass('active')){
  			 var x = e.pageX;
  			 var width = document.documentElement.clientWidth;
  			 if (x < width - 275){
